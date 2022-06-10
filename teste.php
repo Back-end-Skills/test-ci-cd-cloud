00197bfc644a6fe666bfbd86d120510665ca722a
Isaque Menezes <isaque-menezes@hotmail.com>
2022-06-10 08:52:53 -0300

    new update ci cd

--- teste.php
+++ teste.php
@@ -18,3 +18,5 @@
  echo "novo yaml job completo em github, mas sem arquivos no destino novas definições! 0804";
 
  echo "novo yaml job completo em github, mas sem arquivos no destino novas definições! 0804";
+
+ echo "novo yaml job completo em github, mas sem arquivos no destino novas definições! 0805";
